0f403dac0efc8fef5b3d2fa76a2c85963ceed87c
eboshare <t.lila@list.ru>
2019-11-27 19:13:32 +0400

    Add example of test output

--- bot.py
+++ bot.py
@@ -2,3 +2,9 @@ from preparation import dispatcher as dp
 
 if __name__ == '__main__':
     dp.start_polling()
+    '''
+    Output:
+    states.member_menu.under_member.voobshe_lol.lol_function
+    states.admin_menu.some_function
+    states.member_menu.first_part.lalala_function
+    '''
